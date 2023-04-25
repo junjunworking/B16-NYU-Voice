d070bc08b9604bffb2f3acff2e5bb9f54626b27b
JessieDuu <jd4448@nyu.edu>
2023-04-24 20:35:08 -0400

    changeName

--- django_project/NYUVoiceapp/urls.py
+++ django_project/NYUVoiceapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="NYUVoiceapp-home"),
     path("CourseReview/", views.CourseReview, name="NYUVoiceapp-CourseReview"),
+    path('login_failed/', views.login_failed, name='login_failed'),
 
 ]
